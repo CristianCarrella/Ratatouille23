5f23764bb6e9755cc0736a1d575981c7f4fc2fd4
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2023-01-07 17:06:37 +0100

    Get request from android to API

--- source_code/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/LoginActivity.java
+++ source_code/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.ratatouille_android;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,7 +9,18 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
+    public String url= "https://randomuser.me/api/";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +28,11 @@ public class LoginActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.login_button);
         EditText emailField = findViewById(R.id.email_field);
         EditText passwordField = findViewById(R.id.password_field);
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,4 +43,30 @@ public class LoginActivity extends AppCompatActivity {
         btn.setOnClickListener(onClickListener);
 
     }
+
+    void run() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+                LoginActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("Prova: " + myResponse);
+                    }
+                });
+
+            }
+        });
+    }
+
 }
