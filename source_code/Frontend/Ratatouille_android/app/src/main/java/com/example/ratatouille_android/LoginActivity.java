e5221da1bac4ccab95f4400aaaa59e20c0948fc3
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2023-01-10 19:23:09 +0100

    Commit

--- source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/LoginActivity.java
+++ source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/LoginActivity.java
@@ -15,12 +15,14 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    public String url= "https://randomuser.me/api/";
+    public String url= "http://192.168.1.47:8080/login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,36 +30,47 @@ public class LoginActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.login_button);
         EditText emailField = findViewById(R.id.email_field);
         EditText passwordField = findViewById(R.id.password_field);
-        try {
-            run();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = String.valueOf(emailField.getText());
                 String password = String.valueOf(passwordField.getText());
+                try {
+                    run(email, password);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         };
         btn.setOnClickListener(onClickListener);
 
     }
 
-    void run() throws IOException {
+    void run(String email, String password) throws IOException {
+        System.out.println("Prova stampa: "  + email + " " + password);
         OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+                .add("email", email)
+                .add("password", password)
+                .build();
         Request request = new Request.Builder()
                 .url(url)
+                .post(formBody)
                 .build();
+
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
+                System.out.println("Prova fallimento");
+                e.getCause();
+                e.printStackTrace();
                 call.cancel();
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 final String myResponse = response.body().string();
+                System.out.println("Prova1");
                 LoginActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
