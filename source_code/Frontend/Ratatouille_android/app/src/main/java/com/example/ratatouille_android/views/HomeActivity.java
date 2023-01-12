e19048a9cbbd35107dd5040965546fdf363a89ae
alfredodand <alfredodan0194@gmail.com>
2023-01-12 20:34:21 +0100

    bottom navigation bar not rounded but floating

--- source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/views/HomeActivity.java
+++ source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/views/HomeActivity.java
@@ -4,36 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.example.ratatouille_android.views.jfragment.FunctionFragment;
 import com.example.ratatouille_android.views.jfragment.HomeFragment;
-import com.example.ratatouille_android.views.jfragment.NotificationFragment;
+import com.example.ratatouille_android.views.jfragment.LogoutFragment;
+import com.example.ratatouille_android.views.jfragment.NoticesFragment;
 import com.example.ratatouille_android.R;
-import com.example.ratatouille_android.views.jfragment.SettingsFragment;
+import com.example.ratatouille_android.views.jfragment.AccountFragment;
 import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    BottomAppBar bottomAppBar;
 
     HomeFragment homeFragment = new HomeFragment();
-    SettingsFragment settingsFragment = new SettingsFragment();
-    NotificationFragment notificationFragment = new NotificationFragment();
+    AccountFragment accountFragment = new AccountFragment();
+    NoticesFragment noticesFragment = new NoticesFragment();
+    FunctionFragment functionFragment = new FunctionFragment();
+    LogoutFragment logoutFragment = new LogoutFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomAppBar = findViewById(R.id.bottomAppBar);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
 
-        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notification);
+        BadgeDrawable badgeDrawable = bottomNavigationView.getOrCreateBadge(R.id.notices);
         badgeDrawable.setVisible(true);
         badgeDrawable.setNumber(8);
 
+
+        bottomNavigationView.setBackground(null);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -42,16 +52,24 @@ public class HomeActivity extends AppCompatActivity {
                     case R.id.home:
                         getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
                         return true;
-                    case R.id.notification:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notificationFragment).commit();
+                    case R.id.notices:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, noticesFragment).commit();
                         return true;
-                    case R.id.settings:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, settingsFragment).commit();
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
+                        return true;
+                    case R.id.logout:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, logoutFragment).commit();
+                        return true;
+                    case R.id.functions:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, functionFragment).commit();
                         return true;
                 }
 
                 return false;
             }
         });
+        
+
     }
 }
--- source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/views/jfragment/FunctionFragment.java
+++ source_code/Frontend/Ratatouille_android/app/src/main/java/com/example/ratatouille_android/views/jfragment/FunctionFragment.java
@@ -10,13 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.ratatouille_android.R;
 
-
-public class NotificationFragment extends Fragment {
+public class FunctionFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        return inflater.inflate(R.layout.fragment_function, container, false);
     }
 }
