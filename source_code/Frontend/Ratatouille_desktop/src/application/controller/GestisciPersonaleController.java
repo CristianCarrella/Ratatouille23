5c14934fa416e1774e765bed411700e3fd96eb3a
alfredodand <alfredodan0194@gmail.com>
2023-01-23 23:17:07 +0100

    gestisci personale, risorse umane e attivita/account half implemented

--- source_code/Frontend/Ratatouille_desktop/src/application/controller/GestisciPersonaleController.java
+++ source_code/Frontend/Ratatouille_desktop/src/application/controller/GestisciPersonaleController.java
@@ -2,12 +2,15 @@ package application.controller;
 
 import java.io.IOException;
 
+import application.driver.UtenteDriver;
+import application.model.Utente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -16,6 +19,11 @@ public class GestisciPersonaleController {
 	private Stage stage;
 	private Scene scene;
 	private Parent parent;
+	private Utente loggedUser = LoginController.loggedUser;
+	private UtenteDriver utenteDriver = new UtenteDriver();
+	
+	@FXML
+	Label nomeCognomeLabel, emailLabel, ruoloLabel;
 
 	public GestisciPersonaleController() {}
 	
@@ -26,4 +34,63 @@ public class GestisciPersonaleController {
 		stage.setScene(scene);
 		stage.show();
 	}
+	
+	public void mostraInformazioniUtente(String nomeCognome, String email, String ruolo) {
+		nomeCognomeLabel.setText(nomeCognome);
+		emailLabel.setText(email);
+		ruoloLabel.setText(ruolo);
+	}
+	
+	public void declassa(ActionEvent actionEvent) {
+		//servono altri parametri
+		if(loggedUser.getRuolo().equals("admin")) {
+			if(ruoloLabel.getText().equals("supervisore")) {
+				//chiede conferma e poi ruolo diventa addetto alla cucina
+			}
+		}
+		if(ruoloLabel.getText().equals("addetto_cucina")) {
+			//chiede conferma e poi ruolo diventa addetto alla sala
+		}
+		if(ruoloLabel.getText().equals("addetto_sala")) {
+			//errorLabel da creare "Non puoi declassare un addetto alla sala, forse volevi sollevarlo dal suo incarico"
+		}
+		if(ruoloLabel.getText().equals("admin")) {
+			//errorLabel da creare "Non puoi declassare un amministratore"
+		}
+	}
+	
+	public void promuovi(ActionEvent actionEvent) {
+		//servono altri parametri
+		if(ruoloLabel.getText().equals("addetto_cucina")) {
+			//chiede conferma e poi ruolo diventa supervisore
+		}
+		if(ruoloLabel.getText().equals("addetto_sala")) {
+			//chiede conferma e poi ruolo diventa addetto alla cucina
+		}
+		if(ruoloLabel.getText().equals("admin")) {
+			//errorLabel da creare "Non puoi promuovere un amministratore, è già al vertice"
+		}
+		if(ruoloLabel.getText().equals("supervisore")) {
+			//errorLabel da creare "Non puoi promuovere un supervisore, ci può essere un solo amministratore per attività"
+		}
+	}
+	
+	public void licenzia(ActionEvent actionEvent) {
+		//servono altri parametri
+		if(loggedUser.getRuolo().equals("admin")) {
+			if(ruoloLabel.getText().equals("supervisore")) {
+				//chiede conferma e poi licenzia
+			}
+		}
+		if(ruoloLabel.getText().equals("addetto_cucina")) {
+			//chiede conferma e poi licenzia
+		}
+		if(ruoloLabel.getText().equals("addetto_sala")) {
+			//chiede conferma e poi licenzia
+		}
+		if(ruoloLabel.getText().equals("admin")) {
+			//errorLabel da creare "Non puoi licenziare un amministratore"
+		}
+	}
+	
 }
