18d30842916fcc198fca360fc33a9257999fe147
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2023-02-06 20:29:37 +0100

    Commit

--- source_code/Frontend/Ratatouille_desktop/src/test/OrdinaMenuControllerTest.java
+++ source_code/Frontend/Ratatouille_desktop/src/test/OrdinaMenuControllerTest.java
@@ -2,13 +2,37 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import application.controller.OrdinaMenuController;
+import application.model.CategoriaMenu;
 
 public class OrdinaMenuControllerTest {
 
+	String [] listaSenzaDuplicati = {"Primi", "Secondi", "Contorni", "Bevande"};
+	String [] listaVuota = null;
+	String [] listaConDuplicati = {"Primi", "Secondi", "Primi", "Bevande"};
+	String [] listaConNull = {"Primi", null, "Primi", "Bevande"};
+	OrdinaMenuController controller;
+
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
+	
+	@Before
+	public void init() {
+		controller = new OrdinaMenuController();
+	}
+	
+	@Test
+	public void parolaPresenteInListaCategoriaSenzaDuplicati() {
+		CategoriaMenu categoriaMenu = controller.removeFromListCategoria("Primi", listaSenzaDuplicati);
+	}
+	
 
 }
