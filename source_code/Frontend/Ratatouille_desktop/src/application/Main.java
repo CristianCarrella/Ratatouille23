a9265d822a651f9ee814baa0f8b145ae959325cd
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2023-03-01 15:16:05 +0100

    FinalCompilation

--- source_code/Frontend/Ratatouille_desktop/src/application/Main.java
+++ source_code/Frontend/Ratatouille_desktop/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
 //	public static String address = "http://ec2-35-180-116-122.eu-west-3.compute.amazonaws.com:5000";
 //	public static String address = "http://37.100.241.91:5000";
-	public static String address = "http://35.180.61.81:5000";
+	public static String address = "http://ec2-13-39-47-160.eu-west-3.compute.amazonaws.com:5000";
 
 	@Override
 	public void start(Stage primaryStage) {
